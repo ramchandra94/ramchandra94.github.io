5ac9ece094514ab7e9932da9bee8f4ff03299b92
Ramachandra <gvramchandra@gmail.com>
2026-06-13 04:34:55 -0400

    Migrate to Astro 6 + Tailwind 4 (PostCSS); move content config; CI Node 22

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://ramchandra94.github.io',
-  integrations: [mdx(), tailwind(), react()],
+  integrations: [mdx(), react()],
 });
